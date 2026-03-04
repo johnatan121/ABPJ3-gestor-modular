b2d7799850f3a9c595daead7fbaefca774f86479
johnatan121 <johnatan121@live.com>
2026-03-03 21:45:42 -0300

    Add employee management functions
    Implement functions to add and remove employees from a team.

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
+equipe = {}
 
+def adicionar_funcionario():
+    no=input(print('digite o nme do funcionario'))
+    de=input(print('digite o departamento'))
+    equipe[no]=de
+
+def remover_funcionario():
+    if no in equipe:
+        remove.pop(equipe)
+
+def menu():
+    print('digite o numero para acao ')
+    print('1 -> adicionar:')
+    print('2 -> remove membro:')
+    print('0 -> sair:')
+    opcao= input('digite uma opcao ')
+    match opcao:
+        case "1":
+            adicionar_funcionario()
+        case "2":
+            remover_funcionario()
+        case "0":
+            print('finalizando')
+            exit()    
+        case _:
+            print(' o pecao invalida')
+
+print(equipe)
+while True:
+    menu()
