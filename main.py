511f90cb66cd179f202d54d3d19d9c4de58354b2
johnatan121 <johnatan121@live.com>
2026-03-05 21:17:36 -0300

    Remove employee management functions
    Removed functions for adding, removing, and listing employees from the main.py file.

--- main.py
+++ main.py
@@ -1,28 +1,4 @@
-equipe = {}
-
-def adicionar_funcionario():
-    """entrada dos dados para cadastro"""
-    nome =input('digite o nome do funcionario ')
-    departamento=input('digite o departamento ')
-    equipe[nome]=departamento
-
-def remover_funcionario():
-    """ faz a remocao do funcionario"""
-    nome=input('digite o nome do funcionario ')
-    if nome in equipe:
-        equipe.pop(nome)
-        return equipe
-    else:
-        print('funcionario nao encontrado ')
-
-def listar_equipe():
-    """Exibe todos os funcionários cadastrados."""
-    print("\n--- Equipe Atual ---")
-    if not equipe:
-        print("Equipe vazia.")
-    else:
-        for nome, departamento in equipe.items:
-            print(f"{nome} - {departamento} ")
+from modulo import *
    
 while True:
     """ Exibe o menu """
