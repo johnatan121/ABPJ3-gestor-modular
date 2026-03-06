8571db870ffdcc58976d027f4666b2bc705d6de0
johnatan121 <johnatan121@live.com>
2026-03-05 21:01:10 -0300

    Refactor employee management functions and menu

--- main.py
+++ main.py
@@ -1,31 +1,46 @@
 equipe = {}
 
 def adicionar_funcionario():
-    no=input(print('digite o nome do funcionario'))
-    de=input(print('digite o departamento'))
-    equipe[no]=de
+    """entra com os dados do funcionaro"""
+    nome =input('digite o nome do funcionario ')
+    departamento=input('digite o departamento ')
+    equipe[nome]=departamento
 
 def remover_funcionario():
-    if no in equipe:
-        remove.pop(equipe)
+    """ faz a remocao do funcionario"""
+    nome=input('digite o nome do funcionario ')
+    if nome in equipe:
+        equipe.pop(nome)
+        return equipe
+    else:
+        print('funcionario nao encontrado ')
 
-def menu():
-    print('digite o numero para acao ')
-    print('1 -> adicionar:')
-    print('2 -> remove membro:')
-    print('0 -> sair:')
-    opcao= input('digite uma opcao ')
+def listar_equipe():
+    """Exibe todos os funcionários cadastrados."""
+    print("\n--- Equipe Atual ---")
+    if not equipe:
+        print("Equipe vazia.")
+    else:
+        for nome, departamento in equipe.items:
+            print(f"{nome} - {departamento} ")
+   
+while True:
+    
+    print('digite o numero para iniciar "acao" ')
+    print('1 -> adicionar: ')
+    print('2 -> remove membro: ')
+    print('3 -> listar equipe: ')
+    print('0 -> sair: ')
+    opcao= input('escolha uma opcao ')
     match opcao:
         case "1":
             adicionar_funcionario()
         case "2":
             remover_funcionario()
+        case "3":
+            listar_equipe()
         case "0":
             print('finalizando')
             exit()    
         case _:
             print(' o pecao invalida')
-
-print(equipe)
-while True:
-    menu()
