c4dc6dad4f0948be8cc7d44db7a7f95ce83bbc4d
johnatan121 <johnatan121@live.com>
2026-03-05 21:16:31 -0300

    Implement employee management functions

--- modulo.py
+++ modulo.py
@@ -1,1 +1,27 @@
 
+equipe = {}
+
+def adicionar_funcionario():
+    """entra com os dados do funcionaro"""
+    nome =input('digite o nome do funcionario ')
+    departamento=input('digite o departamento ')
+    equipe[nome]=departamento
+
+def remover_funcionario():
+    """ faz a remocao do funcionario"""
+    nome=input('digite o nome do funcionario ')
+    if nome in equipe:
+        equipe.pop(nome)
+        return equipe
+    else:
+        print('funcionario nao encontrado ')
+
+def listar_equipe():
+    """Exibe todos os funcionários cadastrados."""
+    print("\n--- Equipe Atual ---")
+    if not equipe:
+        print("Equipe vazia.")
+    else:
+        for nome, departamento in equipe.items():
+            print(f"{nome} - {departamento} ")
+   
